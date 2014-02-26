e38353172bddafbf55631ec269e4d7f300befc8e
Bregell <johan_bregell@hotmail.com>
2014-02-26 16:34:07 +0100

    Lab 2 Part 1

--- application.c
+++ application.c
@@ -9,18 +9,10 @@ PPIM port_struct = 0x0240;
 
 typedef struct {
 	Object super;
-	int state;
 	int period;
-} Generator;
+} Tone;
 
-Generator gen = { initObject(), 0, 500 };
-
-typedef struct {
-	Object super;
-	int range;
-} Load;
-
-Load load = { initObject(), 0 };
+Tone tone = { initObject(), 500 };
 
 typedef struct {
     Object super;
@@ -40,7 +32,6 @@ int indices[32] = {0,2,4,0,0,2,4,0,4,5,7,4,5,7,7,9,7,5,4,0,7,9,7,5,4,0,0,-5,0,0,
 int periods[25] = {2025,1911,1804,1703,1607,1517,1432,1351,1276,1204,1136,1073,1012,956,902,851,804,758,716,676,638,602,568,536,506};
 int max_index = 14;
 int min_index = -10;
-Time wcet_toggle, wcet_load;
 int timecounter = 0; 
 char buffer[20];
 char strOut[80];
@@ -48,33 +39,11 @@ char strOut[80];
 Serial sci0 = initSerial(SCI_PORT0, &app, reader);
 Can can0 = initCan(CAN0BASE, &app, receiver);
 
-void toggle(Generator *self, int unused){
-	int i; 
-	Time start, diff;
-	start = CURRENT_OFFSET();
+void play(Tone *self, int unused){
 	port_struct -> ptp = !port_struct -> ptp;
-	diff = CURRENT_OFFSET() - start;
-	if(wcet_toggle < diff){
-		wcet_toggle = diff;
-	}
-	SEND(USEC((self->period)),USEC(100), self, toggle, 0);
-	//SEND(USEC((self->period)),0, self, toggle, 0);
+	SEND(USEC(self->period), USEC(100), self, play, 0);
 }
 
-void fakeLoad(Load *self, int range){
-	int i;
-	Time start, diff;
-	start = CURRENT_OFFSET();
-	for(i = 0; i <= range; i++){
-		
-	}
-	diff = CURRENT_OFFSET() - start;
-	if(wcet_load < diff){
-		wcet_load = diff;
-	}
-	SEND(USEC(1300), USEC(1300), self, fakeLoad, background_loop_range);
-	//SEND(USEC(1300), 0, self, fakeLoad, background_loop_range);
-}
 
 void receiver(App *self, int unused) {
     CANMsg msg;
@@ -89,51 +58,31 @@ void reader(App *self, int c) {
     Time gb;
     SCI_WRITE(&sci0, "Rcv: \'");
     if (c == 'f'){
-		sum = 0;
-		counter = 0;
+	sum = 0;
+	counter = 0;
+	background_loop_range = sum;
+	SCI_WRITE(&sci0, "Reset program");
+    }
+    else{
+	if ((c == 'e') && (counter != 19)){
+		sum = atoi(buffer);
 		background_loop_range = sum;
-		SCI_WRITE(&sci0, "Reset program");
-    } else {
-		if (c == 'i') {
-			background_loop_range = background_loop_range + 20;
-			sprintf(strOut,"%i", background_loop_range);
-			SCI_WRITE(&sci0, strOut);
-		} else if (c == 'd') {
-			background_loop_range = background_loop_range - 20;
-			sprintf(strOut,"%i", background_loop_range);
-			SCI_WRITE(&sci0, strOut);
-		} else if (c == 'p') {
-			buff1 = USEC_OF(wcet_toggle);
-			buff2 = USEC_OF(wcet_load);
-			sprintf(strOut,"T:%d, L:%d", buff1, buff2);
-			SCI_WRITE(&sci0, strOut);	
-		} else if (c == 'r') {
-			wcet_toggle = USEC(0);
-			wcet_load = USEC(0);
-		} else if ((c == 'e') && (counter != 19)){
-			sum = atoi(buffer);
-			background_loop_range = sum;
-			/*if ((sum <= 5) && (sum >= -5)){
-				for(i=0;i<32;i++){
-					periodIndex = indices[i] + 10 + sum;
-					sprintf(strOut, "The period for indice %i in key %i %is %i us\n", indices[i], sum, periods[periodIndex]);
-					SCI_WRITE(&sci0, strOut);
-				}
-			}
-			else{
-				sprintf(strOut, "The key %i is out of range ", sum);
-				SCI_WRITE(&sci0, strOut);
-			};*/
-			counter = 0;
+		if ((sum <= -10) && (sum >= 14)){
+			&tone -> period = periods[indices[sum]];
 		}
 		else{
-			SCI_WRITECHAR(&sci0, c);
-			buffer[counter] = c;
-			buffer[counter+1] = '\0';
-			counter = counter + 1;
-			sprintf(strOut,"%i", counter);
+			sprintf(strOut, "The key %i is out of range ", sum);
 			SCI_WRITE(&sci0, strOut);
 		};
+		counter = 0;
+	} else {
+		SCI_WRITECHAR(&sci0, c);
+		buffer[counter] = c;
+		buffer[counter+1] = '\0';
+		counter = counter + 1;
+		sprintf(strOut,"%i", counter);
+		SCI_WRITE(&sci0, strOut);
+	};
     };
     SCI_WRITE(&sci0, "\'\n");
 }
@@ -147,9 +96,8 @@ void startApp(App *self, int arg) {
     
     //Set direction status of port p
     port_struct -> ddrp = 0xFF; 
-    //Start the loop
-    BEFORE(USEC(100), &gen, toggle, 0);
-    BEFORE(USEC(1300), &load, fakeLoad, background_loop_range); 
+    //Start loop
+    BEFORE(USEC(100), &tone, play, 0);
     
     //CAN stuffz
     CAN_INIT(&can0);
